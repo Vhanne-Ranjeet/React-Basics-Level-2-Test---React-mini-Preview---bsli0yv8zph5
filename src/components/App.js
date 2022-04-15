dc96f0bcef59b3eb1c9c2649d53935dfb634c89f
Vhanne-Ranjeet <ranjeetvhanne1234@gmail.com>
2022-04-15 21:56:53 +0530

    final2

--- src/components/App.js
+++ src/components/App.js
@@ -2,20 +2,21 @@ import React, { useState } from 'react'
 import '../styles/App.css';
 import { Preview } from './Preview';
 const App = () => {
-  const [content,setContent]=useState("hello world");
-  const [padding,setPadding]=useState(5);
-  const [size,setSize]=useState(10);
+  const [content, setContent] = useState("hello world");
+  const [padding, setPadding] = useState(5);
+  const [size, setSize] = useState(10);
   return (
     <div id="main">
       <div>
-        Content<input type="text" value={content} id="contentInput" onChange={(e)=>{
-           setContent(e.target.value);
-        }}></input>
-        Padding<input type="number" value={padding} id="paddingInput" onChange={(e)=>{setPadding(e.target.value)}}></input>
-        Font Size<input type="number" value={size} id="fontSizeInput" onChange={(e)=>{ setSize(e.target.value)
-        }}></input>
+        Content<input type="text" value={content} id="contentInput" onChange={(e) => {
+          setContent(e.target.value);
+        }}></input><br />
+        Padding<input type="number" value={padding} id="paddingInput" onChange={(e) => { setPadding(e.target.value) }}></input><br />
+        Font Size<input type="number" value={size} id="fontSizeInput" onChange={(e) => {
+          setSize(e.target.value)
+        }}></input><br />
       </div>
-       <Preview fontSize={size} content={content} padding={padding}/>
+      <Preview fontSize={size} content={content} padding={padding} />
     </div>
   )
 }
