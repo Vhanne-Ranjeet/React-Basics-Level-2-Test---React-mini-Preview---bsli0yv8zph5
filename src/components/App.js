57eeccdfd6ab4489228b415cc96ae132fe9f9557
Vhanne-Ranjeet <ranjeetvhanne1234@gmail.com>
2022-04-17 15:31:42 +0530

    ans1

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import '../styles/App.css';
 import { Preview } from './Preview';
 const App = () => {
-  const [content, setContent] = useState("hello newton");
+  const [content, setContent] = useState("hello world");
   const [padding, setPadding] = useState(5);
   const [fontSize, setSize] = useState(10);
   return (
@@ -12,7 +12,7 @@ const App = () => {
           setContent(e.target.value);
         }}></input><br />
         Padding<input type="number" value={padding} id="paddingInput" onChange={(e) => { setPadding(e.target.value) }}></input><br />
-        Font Size<input type="number" value={size} id="fontSizeInput" onChange={(e) => {
+        Font Size<input type="number" value={fontSize} id="fontSizeInput" onChange={(e) => {
           setSize(e.target.value)
         }}></input><br />
       </div>
